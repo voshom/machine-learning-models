aa02dfc8a5f22b5bfd598b6818d09423877aa77e
voshom <262602559+voshom@users.noreply.github.com>
2026-03-12 00:22:11 +1000

    Initial commit

---
+++ parser.js
@@ -0,0 +1,10 @@
+
+export const generateRandomToken = (length = 32) => {
+  const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  let token = '';
+  for (let i = 0; i < length; i++) {
+    token += chars.charAt(Math.floor(Math.random() * chars.length));
+  }
+  return token;
+};
+
